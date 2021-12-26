f5bdf270873e10cbe65a3d8cc7e9424a32d9e272
mhendricks96 <mhendricks96@yahoo.com>
2021-12-26 15:44:08 -0800

    script will current grab links from as many pages on infoplease as you request

--- base_crawler.py
+++ base_crawler.py
@@ -2,40 +2,37 @@ import requests
 from bs4 import BeautifulSoup
 
 
-# startUrl = 'https://www.infoplease.com/search/guatemala'
-# links = []
-
-def get_article_links(startURL):
+def get_links_on_page(url):
   links = []
-  max_pages = 1
+  page = requests.get(url).text
+  soup = BeautifulSoup(page, 'lxml')
+  section = soup.find('section', {'id':'mainaside'})
+  for link in section.findAll(('a')):
+    href = link.get('href')
+    if 'guatemala' in href:
+      # print('saved',href)
+      links.append(href)
+  return links
+
+
+def get_article_links(beginURL):
+  links = []
+  max_pages = 5
   current_page = 0
+  next_page = 1
+  links.extend(get_links_on_page(beginURL))
+  # get_links_on_page(startURL)
+  # print(startURL)
 
-  def get_links_on_page(url):
-    page = requests.get(url).text
-    soup = BeautifulSoup(page, 'lxml')
-    section = soup.find('section', {'id':'mainaside'})
-    for link in section.findAll(('a')):
-      href = link.get('href')
-      if 'guatemala' in href:
-        print('saved',href)
-        links.append(href)
-
-  def find_next_page():
-    next_page = soup.find('li', {'class':'pager__item page__item--next'}).find('a').get('href')
-    
-    if next_page:
-      return next_page
-    else:
-      return False
-
-  
-  get_links_on_page(startURL)
-
-  if current_page < max_pages:
-    new_link = find_next_page()
+  while current_page < max_pages:
+    # print(beginURL)
+    new_link = f'{beginURL}?page={str(next_page)}'
     if new_link:
       current_page += 1
-      get_links_on_page(startUrl + new_link)
+      next_page += 1
+      links.extend(get_links_on_page(new_link))
+      # currentUrl = new_link
+      # startUrl = new_link
   
   for link in links:
     print(link)
